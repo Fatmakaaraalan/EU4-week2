87a0d9d643f855b6ba11d1eb1e2f41c9de758c74
Fatmakaraalan <fatmauk@hotmail.com>
2021-02-06 16:19:18 +0000

    completed the tc001 with few utility and some assertions

--- src/test/java/TestCase001.java
+++ src/test/java/TestCase001.java
@@ -1,5 +1,6 @@
 public class TestCase001 {
 
-    //i am about to write some awsome code for this test case
+    //i am about to write some awesome code for this test case
     //writing awesome method to reverse a string
+    // completed this awesome test cases ready  to merge
 }
